c056a92c5b183d38b822507f6316dd69d2c37ff7
Lain-dono <lain.dono@gmail.com>
2013-09-24 14:23:56 +0400

    добвален grunt

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+	var srcFiles = ['src/intro.js', 'src/yukiscript.js', 'src/outro.js'],
+		metaFile = grunt.file.read('yukiscript.meta.js');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				banner: metaFile,
+			},
+			dist: {
+				src: srcFiles,
+				dest: 'yukiscript.user.js',
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('default', ['concat']);
+};
