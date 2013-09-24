a74549411a9ee7bd81e1a89f621451dd5235d27c
Lain-dono <lain.dono@gmail.com>
2013-09-24 15:12:55 +0400

    вынес некоторые ф-ии в utils.js

--- src/yukiscript.js
+++ src/yukiscript.js
@@ -14,139 +14,6 @@ var yukiSaysWeFocused = true,
     yukiRemoveExif = true,
     yukiRemoveFileName = true;
 
-parseUrl = function(url) {
-    m = (url || document.location.href).match(/https?:\/\/([^\/]+)\/([^\/]+)\/((\d+)|res\/(\d+)|\w+)(\.x?html)?(#i?(\d+))?/);
-    return m ? {
-        host: m[1],
-        board: m[2],
-        page: m[4],
-        thread: m[5],
-        pointer: m[8]
-    } : {};
-};
-
-var makeRandId = function(size) {
-    var text = "";
-    var possible = "0123456789abcdef";
-
-    for (var i = 0; i < size; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
-};
-
-var arrayBufferDataUri = function(raw) {
-    var base64 = '';
-    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-
-    var bytes = new Uint8Array(raw);
-    var byteLength = bytes.byteLength;
-    var byteRemainder = byteLength % 3;
-    var mainLength = byteLength - byteRemainder;
-
-    var a, b, c, d;
-    var chunk;
-
-    // Main loop deals with bytes in chunks of 3
-    for (var i = 0; i < mainLength; i = i + 3) {
-        // Combine the three bytes into a single integer
-        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
-
-        // Use bitmasks to extract 6-bit segments from the triplet
-        a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
-        b = (chunk & 258048) >> 12; // 258048   = (2^6 - 1) << 12
-        c = (chunk & 4032) >> 6; // 4032     = (2^6 - 1) << 6
-        d = chunk & 63; // 63       = 2^6 - 1
-        // Convert the raw binary segments to the appropriate ASCII encoding
-        base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
-    }
-
-    // Deal with the remaining bytes and padding
-    if (byteRemainder == 1) {
-        chunk = bytes[mainLength];
-
-        a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
-        // Set the 4 least significant bits to zero
-        b = (chunk & 3) << 4; // 3   = 2^2 - 1
-        base64 += encodings[a] + encodings[b] + '==';
-    } else if (byteRemainder == 2) {
-        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
-
-        a = (chunk & 16128) >> 8; // 16128 = (2^6 - 1) << 8
-        b = (chunk & 1008) >> 4; // 1008  = (2^6 - 1) << 4
-        // Set the 2 least significant bits to zero
-        c = (chunk & 15) << 2; // 15    = 2^4 - 1
-        base64 += encodings[a] + encodings[b] + encodings[c] + '=';
-    }
-
-    return base64;
-};
-
-var jpegStripExtra = function(input) {
-    // result e.target.result;
-
-    // Decode the dataURL    
-    var binary = atob(input.split(',')[1]);
-    // Create 8-bit unsigned array
-    var array = [];
-    for (var i = 0; i < binary.length; i++) {
-        array.push(binary.charCodeAt(i));
-    }
-
-    var orig = new Uint8Array(array);
-    var outData = new ArrayBuffer(orig.byteLength);
-    var output = new Uint8Array(outData);
-    var posO = 2,
-        posT = 2;
-
-    output[0] = orig[0];
-    output[1] = orig[1];
-
-    while (!(orig[posO] === 0xFF && orig[posO + 1] === 0xD9) && posO <= orig.byteLength) {
-        if (orig[posO] === 0xFF && orig[posO + 1] === 0xFE) {
-            posO += 2 + orig[posO + 2] * 256 + orig[posO + 3];
-        } else if (orig[posO] === 0xFF && (orig[posO + 1] >> 4) === 0xE) {
-            posO += 2 + orig[posO + 2] * 256 + orig[posO + 3];
-        } else if (orig[posO] === 0xFF && orig[posO + 1] === 0xDA) {
-            var l = (2 + orig[posO + 2] * 256 + orig[posO + 3]);
-            for (var i = 0; i < l; i++) {
-                output[posT++] = orig[posO++];
-            }
-            while (!(orig[posO] === 0xFF && orig[posO + 1] !== 0 && orig[posO + 1] < 0xD0 && orig[posO + 1] > 0xD7) && posO <= orig.byteLength) {
-                output[posT++] = orig[posO++];
-            }
-        } else {
-            var l = (2 + orig[posO + 2] * 256 + orig[posO + 3]);
-            for (var i = 0; i < l; i++) {
-                output[posT++] = orig[posO++];
-            }
-        }
-    }
-
-    output[posT] = orig[posO];
-    output[posT + 1] = orig[posO + 1];
-
-    output = new Uint8Array(outData, 0, posT + 2);
-
-    return "data:image/Jpeg;base64," + arrayBufferDataUri(output);
-};
-
-var difference = function(array1, array2) {
-    var result = [];
-
-    if (array2.length === 0) {
-        return array1;
-    }
-
-    for (var i = 0; i < array1.length; i++) {
-        if (array2.indexOf(array1[i]) == -1) {
-            result.push(array1[i]);
-        }
-    }
-
-    return result;
-};
-
 var yukiPleaseReplyLinks2 = function() {
     'use strict';
 
@@ -405,18 +272,6 @@ var yukiMakeReplyForm = function(click, board, tid, pid) {
     }
 };
 
-
-
-var bytesMagnitude = function(bytes) {
-    if (bytes < 1024) {
-        return bytes + ' B';
-    } else if (bytes < 1024 * 1024) {
-        return (bytes / 1024).toFixed(2) + ' KB';
-    } else {
-        return (bytes / 1024 / 1024).toFixed(2) + ' MB';
-    }
-};
-
 var yukiAddFile = function(evt, b) {
     var files = evt.target.files; // FileList object
 
@@ -485,20 +340,6 @@ var yukiAddFile = function(evt, b) {
         reader.readAsDataURL(f);
     }
 };
-
-function dataURLtoBlob(dataURL, dataType) {
-    // Decode the dataURL    
-    var binary = atob(dataURL.split(',')[1]);
-    // Create 8-bit unsigned array
-    var array = [];
-    for (var i = 0; i < binary.length; i++) {
-        array.push(binary.charCodeAt(i));
-    }
-    // Return our Blob object
-    return new Blob([new Uint8Array(array)], {
-        type: dataType
-    });
-}
 
 var yukiPleasePost = function() {
     var formData = $('#yukipostform').serializeArray(),
@@ -651,14 +492,6 @@ var yukiAttachCapcha = function(el) {
 };
 
 
-var supports_html5_storagefunction = function() {
-    try {
-        return 'localStorage' in window && window['localStorage'] !== null;
-    } catch (e) {
-        return false;
-    }
-};
-
 var yukiGetLocalStorageValue = function(name, deflt) {
     if (supports_html5_storagefunction() && name in localStorage) {
         var v = localStorage.getItem(name);
@@ -703,8 +536,4 @@ var yukiSetNewOptions = function(el) {
     }
 };
 
-function checkEnter(e) {
-    e = e || event;
-    return (e.keyCode || e.which || e.charCode || 0) !== 13;
-}
 
