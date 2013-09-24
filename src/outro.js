275c5a82c9ee29f7f985fee7ce1498589942a8cc
Lain-dono <lain.dono@gmail.com>
2013-09-24 14:27:04 +0400

    перемещение всего в src, добавка .meta.js, обертка

---
+++ src/outro.js
@@ -0,0 +1,7 @@
+// MAIN END
+};
+
+var script = document.createElement("script");
+script.textContent = "(" + (main.toString()) + ")();";
+document.body.appendChild(script);
+
--- src/yukiscript.js
+++ src/yukiscript.js
@@ -1,17 +1,3 @@
-// ==UserScript==
-// @name         ЮкиСкрипт
-// @namespace    udp://insomnia/*
-// @version      0.0.7
-// @description  enter something useful
-// @match        http://dobrochan.com/*/*
-// @match        http://dobrochan.ru/*/*
-// @include      http://dobrochan.com/*/*
-// @include      http://dobrochan.ru/*/*
-// @copyright    2013+, You
-// @updateURL    https://userscripts.org/scripts/source/176182.user.js
-// @grant        none
-// ==/UserScript==
-
 var yukiSaysWeFocused = true,
     emptyIconData = '',
     dobrochanIconData = '',
